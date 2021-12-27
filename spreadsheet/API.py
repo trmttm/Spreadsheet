756132a468db161a46bf0687d6036633b3342ce7
yamaka <t.yamaka.1213@gmail.com>
2021-12-27 22:02:44 +0400

    comment added again

--- spreadsheet/API.py
+++ spreadsheet/API.py
@@ -11,6 +11,7 @@ from .Presenters import Presenters
 
 class Spreadsheet(SpreadsheetABC):
     def __init__(self):
+        # commend added again
         entities = Entities()
         presenters = Presenters()
         gateways = Gateways(entities)
